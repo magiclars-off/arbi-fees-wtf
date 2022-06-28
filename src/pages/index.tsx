53a49fb6029285c7e8b113bd36e95dfb8843c0c1
Lars Sinke <magiclars@protonmail.com>
2022-06-28 20:01:42 +0200

    Few updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { BigNumber, constants } from 'ethers';
 import { formatEther } from 'ethers/lib/utils';
 import type { NextPage } from 'next';
+import Head from 'next/head';
 import { useEffect, useMemo, useState } from 'react';
 import { useAccount } from 'wagmi';
 import { ARBISCAN_KEY } from '../config';
@@ -24,7 +25,7 @@ const Home: NextPage = () => {
   const totals = useMemo(() => {
     return txs.reduce(
       (acc, tx: Tx) => ({
-        failed: tx.isError === '1' ? acc.failed + 1 : acc.failed,
+        failed: tx.isError === '1' ? acc.failed++ : acc.failed,
         failedGas: tx.isError === '1' ? acc.failedGas.add(BigNumber.from(tx.gasUsed).mul(tx.gasPrice)) : acc.failedGas,
         totalGas: acc.totalGas.add(tx.gasUsed),
         totalEth: acc.totalEth.add(BigNumber.from(tx.gasUsed).mul(tx.gasPrice)),
@@ -41,45 +42,64 @@ const Home: NextPage = () => {
   }, [txs]);
 
   return (
-    <div className="max-w-[1200px] px-[5vw] my-[15vh]  mx-auto text-center text-4xl font-medium text-ellipsis leading-[3rem]">
-      <p>
-        {`You've spent `}
-        <span className="highlight">
-          {totals.totalEth.gt(constants.Zero) ? `Ξ${formatEther(totals.totalEth).substring(0, 7)}` : 'Ξ0'}
-        </span>
-        {` on gas. Right now, that's`}{' '}
-        <span className="highlight">
-          {totals.totalEth.gt(constants.Zero)
-            ? `$${formatPrice(BigNumber.from(totals.totalEth).mul(Math.round(+ethPrice * 100)).div(100).toString())}`
-            : '$0'}
-        </span>
-        .
-      </p>
-      <br />
-      <p>
-        You used <span className="highlight">{abbreviatePrice(totals.totalGas.toString())} </span>
-        gas to send
-        <span className="highlight"> {txs.length} </span>
-        transactions, with an average price of
-        <span className="highlight">
-          {' '}
-          {totals.totalPrice.gt(constants.Zero)
-            ? `${Number(totals.totalPrice.div(txs.length).div('1000000')) / 1000}`
-            : '0'}{' '}
-        </span>{' '}
-        Gwei.
-      </p>
-      <br />
-      <p>
-        <span className="highlight">{totals?.failed} </span>
-        of them failed, costing you{' '}
-        <span className="highlight">
-          {' '}
-          {totals.failedGas.gt(constants.Zero) ? `Ξ${Number(formatEther(totals.failedGas)).toFixed(5)}` : 'Ξ0'}
-        </span>
-        .
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Arbi Fees WTF</title>
+        <meta name="title" content={'Arbi Fees WTF'} />
+        <meta name="description" content="Arbitrum historical gas fee checker" />
+        <meta name="keywords" content="gas, ethereum, arbitrum, fees, community" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://arbi-fees.wtf/" />
+        <meta property="og:title" content="Arbi Fees WTF" />
+        <meta property="og:description" content="Arbitrum historical gas fee checker" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Arbi Fees WTF" />
+        <meta property="twitter:description" content="Arbitrum historical gas fee checker" />
+      </Head>
+      <div className="max-w-[1200px] px-[5vw] my-[15vh]  mx-auto text-center text-4xl font-medium text-ellipsis leading-[3rem]">
+        <p>
+          {`You've spent `}
+          <span className="highlight">
+            {totals.totalEth.gt(constants.Zero) ? `Ξ${formatEther(totals.totalEth).substring(0, 7)}` : 'Ξ0'}
+          </span>
+          {` on gas. Right now, that's`}{' '}
+          <span className="highlight">
+            {totals.totalEth.gt(constants.Zero)
+              ? `$${formatPrice(
+                  BigNumber.from(totals.totalEth)
+                    .mul(Math.round(+ethPrice * 100))
+                    .div(100)
+                    .toString()
+                )}`
+              : '$0'}
+          </span>
+          .
+        </p>
+        <br />
+        <p>
+          You used <span className="highlight">{abbreviatePrice(totals.totalGas.toString())} </span>
+          gas to send
+          <span className="highlight"> {txs.length} </span>
+          transactions, with an average price of
+          <span className="highlight">
+            {' '}
+            {totals.totalPrice.gt(constants.Zero)
+              ? `${Number(totals.totalPrice.div(txs.length).div('1000000')) / 1000}`
+              : '0'}
+          </span>{' '}
+          Gwei.
+        </p>
+        <br />
+        <p>
+          <span className="highlight">{totals?.failed} </span>
+          of them failed, costing you{' '}
+          <span className="highlight">
+            {' '}
+            {totals.failedGas.gt(constants.Zero) ? `Ξ${Number(formatEther(totals.failedGas)).toFixed(5)}` : 'Ξ0'}
+          </span>.
+        </p>
+      </div>
+    </>
   );
 };
 
